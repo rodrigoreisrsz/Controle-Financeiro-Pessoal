d5ac206f6f5e331cd0595116d8ef97c9b0867afe
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-10 20:27:55 -0300

    adiciona @entity ao saldo

--- src/main/java/com/reis/financeiro/entities/Saldo.java
+++ src/main/java/com/reis/financeiro/entities/Saldo.java
@@ -1,9 +1,23 @@
 package com.reis.financeiro.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class Saldo {
+    @Id
+    private Long id;
     private BigDecimal saldo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public BigDecimal getSaldo() {
         return saldo;
