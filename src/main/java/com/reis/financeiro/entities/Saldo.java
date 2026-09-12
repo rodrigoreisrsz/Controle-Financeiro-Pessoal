44713e7d96681fdaa83ed83689009ff99db13801
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-11 21:42:25 -0300

    torna classe saldo uma entidade

--- src/main/java/com/reis/financeiro/entities/Saldo.java
+++ src/main/java/com/reis/financeiro/entities/Saldo.java
@@ -1,6 +1,8 @@
 package com.reis.financeiro.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 @Entity
 public class Saldo {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal saldo;
 
