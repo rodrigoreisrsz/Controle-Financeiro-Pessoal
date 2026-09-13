1fd208dc93b933b5085410178e47618ae794d0ab
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-12 21:21:43 -0300

    add

--- src/main/java/com/reis/financeiro/exceptions/RegistroNotFoundException.java
+++ src/main/java/com/reis/financeiro/exceptions/RegistroNotFoundException.java
@@ -1,10 +1,8 @@
 package com.reis.financeiro.exceptions;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 
-@ControllerAdvice
 public class RegistroNotFoundException extends RuntimeException {
     public RegistroNotFoundException(){
-        System.out.println("Registro não encontrado!");
+        super("Registro não encontrado!");
     }
 }
