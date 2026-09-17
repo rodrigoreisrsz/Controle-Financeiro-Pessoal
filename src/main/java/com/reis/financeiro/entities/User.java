5efcded891457980b1dca1b731ae66806de0fca7
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-17 14:18:57 -0300

    add anotações da classe user

--- src/main/java/com/reis/financeiro/entities/User.java
+++ src/main/java/com/reis/financeiro/entities/User.java
@@ -1,12 +1,24 @@
 package com.reis.financeiro.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private String password;
 
     public User(String name, String password) {
         this.name = name;
         this.password = password;
+    }
+
+    public User() {
+
     }
 
     public String getName() {
