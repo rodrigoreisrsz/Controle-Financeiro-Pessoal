2d72c4b5c9827b83597a79356fdd224cf923da6f
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-17 22:43:14 -0300

    adiciona metodo de login

--- src/main/java/com/reis/financeiro/repository/UserRepository.java
+++ src/main/java/com/reis/financeiro/repository/UserRepository.java
@@ -5,4 +5,5 @@ import com.reis.financeiro.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByName(String name);
 }
