559c5ded0b5d09492c901641dea45dc0c252d56b
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-24 21:29:49 -0300

    userdto retorna apenas name

--- src/main/java/com/reis/financeiro/controllers/RegistroController.java
+++ src/main/java/com/reis/financeiro/controllers/RegistroController.java
@@ -29,6 +29,7 @@ public class RegistroController {
     }
     @PostMapping
     public Registro criar(@RequestBody @Valid RegistroCreateDTO registroCreate){
+
         return service.adicionarRegistro(registroCreate.getUserId(), registroCreate.getNome(),  registroCreate.getValor(), registroCreate.getDescricao(), registroCreate.getData(), registroCreate.getTipoRegistro());
     }
     @DeleteMapping("/{id}")
