8d6f71af37b56bb510244e16029927d7eb1480eb
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-25 14:27:36 -0300

    corrige user

--- src/main/java/com/reis/financeiro/entities/User.java
+++ src/main/java/com/reis/financeiro/entities/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "usuario")
-public class User extends UserResponse {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
